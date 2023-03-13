2229c8f39cd7c76f12b957e6ddcdd38bd1fa805c
Vladislav <wlladisla@gmail.com>
2023-03-14 00:33:34 +0300

    feat: add routing

---
+++ react_components/src/Error.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Error extends React.Component {
+  render() {
+    return <h2> Oops 404 </h2>;
+  }
+}
+
+export default Error;
--- react_components/src/main.tsx
+++ react_components/src/main.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.scss";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Error from "./Error";
+import AboutUs from "./AboutUs";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/about_us" element={<AboutUs />} />
+        <Route path="/" element={<App />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
