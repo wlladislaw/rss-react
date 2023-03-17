8f3d4267337d33338e4c853a17ac1f4afd9bf72c
Vladislav <wlladisla@gmail.com>
2023-03-18 02:08:45 +0300

    feat: add test for App component

---
+++ react_components/src/App.test.tsx
@@ -0,0 +1,23 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import App from './App';
+import NavBar from './NavBar';
+
+describe('App', () => {
+  it('has input with placeholder', () => {
+    render(<App />);
+    screen.debug();
+    expect(screen.getByRole('textbox')).toBeInTheDocument();
+    expect(screen.getByPlaceholderText(/search/i)).toBeInTheDocument();
+    expect(screen.getByDisplayValue('')).toBeInTheDocument();
+  });
+});
+
+// describe('NavBar', () => {
+//   it('should render page About Us on click', async () => {
+//     render(<NavBar />);
+//     await userEvent.click(screen.getByRole('About Us'));
+//     expect(screen.getAllByText(/About Us !/i)).toBeInTheDocument();
+//   });
+// });
