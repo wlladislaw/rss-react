427f3b4a65360377920921b17f4ddb9ce42e781e
Vladislav <wlladisla@gmail.com>
2023-03-30 01:45:44 +0300

    refactor: change components on hooks

--- react_components/src/components/About/AboutUs.tsx
+++ react_components/src/components/About/AboutUs.tsx
@@ -1,7 +1,3 @@
-import React from 'react';
-class AboutUs extends React.Component {
-  render() {
-    return <h2>About Us !</h2>;
-  }
+export default function MainPage() {
+  return <h2>About Us !</h2>;
 }
-export default AboutUs;
