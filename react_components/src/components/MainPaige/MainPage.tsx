90c9326c2ac66f184e55244e85a8d3ce16ac743a
Vladislav <wlladisla@gmail.com>
2023-03-29 22:46:03 +0300

    init: start hooks

--- react_components/src/components/MainPaige/MainPage.tsx
+++ react_components/src/components/MainPaige/MainPage.tsx
@@ -1,17 +1,11 @@
-import React from 'react';
-
 import Cards from '../CardsList/CardsList';
 import SearchBar from '../SearchBar/SearchBar';
 
-class MainPage extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <SearchBar prop={''} />
-        <Cards />
-      </div>
-    );
-  }
+export default function MainPage() {
+  return (
+    <div className="App">
+      <SearchBar prop={''} />
+      <Cards />
+    </div>
+  );
 }
-
-export default MainPage;
--- react_components/src/components/NavBar/NavBar.tsx
+++ react_components/src/components/NavBar/NavBar.tsx
@@ -1,28 +1,23 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
 
-class NavBar extends React.Component {
-  render() {
-    return (
-      <div className="navbar">
-        <div className="navbar_main">
-          <Link to="/">
-            <h1>Main</h1>
-          </Link>
-        </div>
-        <div className="navbar_forms">
-          <Link to="/forms">
-            <h2>Forms</h2>
-          </Link>
-        </div>
-        <div className="navbar_aboutUs">
-          <Link to="/about_us">
-            <h2>About Us</h2>
-          </Link>
-        </div>
+export default function NavBar() {
+  return (
+    <div className="navbar">
+      <div className="navbar_main">
+        <Link to="/">
+          <h1>Main</h1>
+        </Link>
       </div>
-    );
-  }
+      <div className="navbar_forms">
+        <Link to="/forms">
+          <h2>Forms</h2>
+        </Link>
+      </div>
+      <div className="navbar_aboutUs">
+        <Link to="/about_us">
+          <h2>About Us</h2>
+        </Link>
+      </div>
+    </div>
+  );
 }
-
-export default NavBar;
