76335b55036b512c132fc5f93cfdb51e172c4eb7
Vladislav <wlladisla@gmail.com>
2023-04-01 01:01:07 +0300

    refactor: change inputs on react hook form

--- react_components/src/components/Form/Form.tsx
+++ react_components/src/components/Form/Form.tsx
@@ -1,19 +1,18 @@
-import {  FormEvent, useState } from 'react';
+import { FormEvent, useState } from 'react';
 import { useForm, useFormState } from 'react-hook-form';
 import { Forms, FormState } from '../../types';
 import FormsList from '../FormsList/FormsList';
 import './Form.scss';
 
 const Form = () => {
-const {register,handleSubmit, reset } = useForm();
+  const { register, handleSubmit, reset } = useForm();
 
-const [forms, setForms] = useState<Forms[]>([]);
-const [isSubmitted, setIsSubmitted] = useState(false);
-const [isValidPhone, setIsValidPhone] = useState(true);
-const [isValidName, setIsValidName] = useState(true);
+  const [forms, setForms] = useState<Forms[]>([]);
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const [isValidPhone, setIsValidPhone] = useState(true);
+  const [isValidName, setIsValidName] = useState(true);
 
-
-  handleSubmit(event: FormEvent<HTMLFormElement>) {
+  function onSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
 
     const validate = (name: string, phone: string) => {
@@ -62,105 +61,103 @@ const [isValidName, setIsValidName] = useState(true);
     event.currentTarget.reset();
 
     setTimeout(() => {
-      this.setState({
-        isSubmited: false,
-      });
+      setIsSubmitted(false);
     }, 3700);
   }
 
-    return (
-      <>
-        {isSubmitted && (
-          <div className="confirmation_message">
-            <h2>Data has been saved! Thanks for order!</h2>
-          </div>
-        )}
-        <form onSubmit={handleSubmit}>
-          <div className="form_container">
-            <div className="form_info">
-              <h3>Information</h3>
-              <label>
-                Your name
-                <input
-                  id="name"
-                  {...register('name', { required: true })}
-                  name="name"
-                  type="text"
-                  placeholder="Name and Last name"
-                  required
-                />
-              </label>
-              <p style={{ color: 'red', margin: 0 }}>{!isValidName ? 'Invalid value' : null}</p>
-
-              <label htmlFor="phone">Phone</label>
+  return (
+    <>
+      {isSubmitted && (
+        <div className="confirmation_message">
+          <h2>Data has been saved! Thanks for order!</h2>
+        </div>
+      )}
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="form_container">
+          <div className="form_info">
+            <h3>Information</h3>
+            <label>
+              Your name
               <input
-                id="phone"
-                ref={this.phoneInput}
-                name="phone"
+                id="name"
+                {...register('name', { required: true })}
+                name="name"
                 type="text"
-                placeholder="+*********"
+                placeholder="Name and Last name"
                 required
               />
-              <p style={{ color: 'red', margin: 0 }}>{!isValidPhone ? 'Invalid value' : null}</p>
-              <br />
-              <label>
-                Date of birthday
-                <input
-                  type="date"
-                  ref={this.dateInput}
-                  defaultValue="1990-01-01"
-                  max={new Date().toISOString().split('T')[0]}
-                  min="1900-01-01"
-                />
-              </label>
-              <br />
-              <label htmlFor="payment">Choose a payment method </label>
-              <select ref={this.selectInput} name="payment">
-                <option value="cash">Cash</option>
-                <option value="online">Online</option>
-                <option value="crypto">Crypto</option>
-              </select>
-              <br />
-              <label>
-                Delivery
-                <input type="checkbox" ref={this.checkboxInput} />
-              </label>
+            </label>
+            <p style={{ color: 'red', margin: 0 }}>{!isValidName ? 'Invalid value' : null}</p>
 
-              <div className="switch_field">
-                <span>Gender</span>
-                <input
-                  type="radio"
-                  ref={this.radioRefMale}
-                  name="switch"
-                  value="male"
-                  defaultChecked
-                />
-                <label htmlFor="radio-one">Male</label>
-                <input type="radio" name="switch" value="female" />
-                <label htmlFor="radio-two">Female</label>
-              </div>
-              <div id="file_upload-container">
-                <label>
-                  <input
-                    type="file"
-                    name="file"
-                    ref={this.fileInput}
-                    id="upload_file"
-                    accept="image/*"
-                  />
-                  <span>Upload avatar</span>
-                </label>
-              </div>
+            <label htmlFor="phone">Phone</label>
+            <input
+              id="phone"
+              {...register('phone', { required: true })}
+              name="phone"
+              type="text"
+              placeholder="+*********"
+              required
+            />
+            <p style={{ color: 'red', margin: 0 }}>{!isValidPhone ? 'Invalid value' : null}</p>
+            <br />
+            <label>
+              Date of birthday
+              <input
+                type="date"
+                {...register('date', { required: true })}
+                defaultValue="1990-01-01"
+                max={new Date().toISOString().split('T')[0]}
+                min="1900-01-01"
+              />
+            </label>
+            <br />
+            <label htmlFor="payment">Choose a payment method </label>
+            <select {...register('payment', { required: true })} name="payment">
+              <option value="cash">Cash</option>
+              <option value="online">Online</option>
+              <option value="crypto">Crypto</option>
+            </select>
+            <br />
+            <label>
+              Delivery
+              <input type="checkbox" {...register('delivery')} />
+            </label>
 
-              <button className="form_btn">Save</button>
+            <div className="switch_field">
+              <span>Gender</span>
+              <input
+                type="radio"
+                {...register('gender')}
+                name="switch"
+                value="male"
+                defaultChecked
+              />
+              <label htmlFor="radio-one">Male</label>
+              <input type="radio" name="switch" value="female" />
+              <label htmlFor="radio-two">Female</label>
             </div>
+            <div id="file_upload-container">
+              <label>
+                <input
+                  type="file"
+                  name="file"
+                  {...register('file')}
+                  id="upload_file"
+                  accept="image/*"
+                />
+                <span>Upload avatar</span>
+              </label>
+            </div>
+
+            <button className="form_btn">Save</button>
           </div>
-        </form>
-        <div className="info_cards">
-          <FormsList forms={forms} />
         </div>
-      </>
-    );
-  }
+      </form>
+      <div className="info_cards">
+        <FormsList forms={forms} />
+      </div>
+    </>
+  );
+};
 
 export default Form;
